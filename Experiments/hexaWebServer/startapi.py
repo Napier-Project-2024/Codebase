8c8f3863ac16bf4c4716ae9fa24e72f93ba5ef94
Chris <c.lavender@pm.me>
2024-03-29 19:26:03 +0000

    added web interface

--- Experiments/hexaWebServer/startapi.py
+++ Experiments/hexaWebServer/startapi.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
 
-from flask import Flask, jsonify
+from flask import Flask, render_template
+import datetime
+
 app = Flask(__name__)
+
+# web interface
+
+@app.route("/")
+
+def index():
+
+    return render_template('index.html')
+
+# API returns a JSON
 
 @app.route('/returnVals')
 def returnVals():
@@ -11,7 +23,7 @@ def returnVals():
     return data
 
 
-# insert a web interface?
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
