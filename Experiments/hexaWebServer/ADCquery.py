2eb75256ffc5bb7a282484ccf39f4c5698165c17
Chris <c.lavender@pm.me>
2024-03-29 16:14:49 +0000

    json

--- Experiments/hexaWebServer/ADCquery.py
+++ Experiments/hexaWebServer/ADCquery.py
@@ -1,3 +1,6 @@
+import json
+
+
 try:
     from ADCPi import ADCPi
 except ImportError:
@@ -12,19 +15,34 @@ except ImportError:
             "Failed to import library from parent folder")
     
 
-def main():
+# def main():
   
+#     adc = ADCPi(0x68, 0x69, 12)
+
+
+#         # read from the ADC channels and print to screen
+#     print("Channel 1: %02f" % adc.read_voltage(1))
+#     print("Channel 2: %02f" % adc.read_voltage(2))
+#     print("Channel 3: %02f" % adc.read_voltage(3))
+#     print("Channel 4: %02f" % adc.read_voltage(4))
+#     print("Channel 5: %02f" % adc.read_voltage(5))
+#     print("Channel 6: %02f" % adc.read_voltage(6))
+#     print("Channel 7: %02f" % adc.read_voltage(7))
+#     print("Channel 8: %02f" % adc.read_voltage(8))
+    
+    
+def main():
+    
     adc = ADCPi(0x68, 0x69, 12)
+    
+    values = {
+        1 : adc.read_voltage(1),
+        2 : adc.read_voltage(2),
+        3 : adc.read_voltage(3),
+        4 : adc.read_voltage(4)        
+    }
+    
+    return json.dumps(values)
 
-
-        # read from the ADC channels and print to screen
-    print("Channel 1: %02f" % adc.read_voltage(1))
-    print("Channel 2: %02f" % adc.read_voltage(2))
-    print("Channel 3: %02f" % adc.read_voltage(3))
-    print("Channel 4: %02f" % adc.read_voltage(4))
-    print("Channel 5: %02f" % adc.read_voltage(5))
-    print("Channel 6: %02f" % adc.read_voltage(6))
-    print("Channel 7: %02f" % adc.read_voltage(7))
-    print("Channel 8: %02f" % adc.read_voltage(8))
 
 main()
