d2c94c84586ab649eeb67796b9948deff6d21cba
Chris <c.lavender@pm.me>
2024-03-30 21:22:01 +0000

    fastapi test

---
+++ Experiments/fastapi/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from starlette.responses import FileResponse 
+import datetime
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    
+    return FileResponse('index.html')
+
+@app.get("/test")
+async def test():
+    def time():
+        now = datetime.datetime.now()    
+        timeString = now.strftime("%Y-%m-%d %H:%M:%S:%f")
+        return timeString
+
+    while True:
+        return time()
+    
+
