7950920489aabcb705aca3ca45f0929b0cab7142
AllanForsyth40582311 <40582311@live.napier.ac.uk>
2024-04-21 16:12:32 +0100

    corrected hexaboot.sh

--- Hexasense_API/hexaboot.sh
+++ Hexasense_API/hexaboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /usr/Hexasense/Hexasense_API && python -m uvicorn main:app --host 0.0.0.0 --port 5000 --reload
+cd /usr/Hexasense/Hexasense_API && python3 -m uvicorn main:app --host 0.0.0.0 --port 5000 --reload
 
